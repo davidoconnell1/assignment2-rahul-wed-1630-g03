f4fc8dcce5275483983f988edd9e46a85f7660b6
104532329 <104532329@student.swin.edu.au>
2025-05-20 22:05:46 +1000

    updates to apply and process_eoi pages

--- project2/process_eoi.php
+++ project2/process_eoi.php
@@ -2,6 +2,23 @@
 session_start();
 require_once('settings.php');
 $conn = mysqli_connect("localhost", "root", "", "project2db");
-if (!$conn) {
+if ($conn) {
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $dob = $_POST['dob'];
+    $gender = $_POST['gender'];
+    $address = $_POST['address'];
+    $suburb = $_POST['suburb'];
+    $state = $_POST['state'];
+    $postcode = $_POST['postcode'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $jobnumber = $_POST['jobnumber'];
+    $experience = $_POST['pythonexperience'];
+
+    echo "$jobnumber";
+
+    //$result = mysqli_query();
+} else {
     die("Connection falied: " . mysqli_connect_error());
 }
