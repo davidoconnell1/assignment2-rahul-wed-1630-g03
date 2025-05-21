6cc98afcb28fdeb25abc4624f970748d10cf9846
davidoconnell1 <105348831@student.swin.edu.au>
2025-05-21 14:40:58 +1000

    updated settings.php

--- project2/settings.php
+++ project2/settings.php
@@ -3,4 +3,10 @@ $host = "localhost";
 $user = "root";
 $pwd = "";
 $sql_db = "project2db";
+
+$conn = mysqli_connect($host, $user, $pwd, $sql_db);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 ?>
